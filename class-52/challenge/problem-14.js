d5ed94ccdeb99bce0b7ec6b51c57828d62709e78
Gabbie-Darlin <gabdarlin@gmail.com>
2026-03-31 22:07:24 +0000

    code challenge JavaScript file modified

--- class-52/challenge/problem-14.js
+++ class-52/challenge/problem-14.js
@@ -19,15 +19,14 @@ if(isNaN(grade)) {
     alert("The final project input should be 'yes' or 'no'");
 
     // business rules (logic)
-    
-} else if(attendance < 75) {
     //Declare local variables
-    let result;
-    let performance;
+
+    let result = " ";
+    let performance = " ";
+} else if(attendance < 75) {
     result = "Fail. insufficient attendance";
     performance = "Insufficient";
-} else {
-    if(grade >= 90) {
+} else if (grade >= 90) {
         result = "PASS";
         performance = "Excellent";
 
@@ -36,21 +35,41 @@ if(isNaN(grade)) {
             result = "PASS";
             performance = "Good";
         } else {
-            result = "remedial exam required."
+            result = "remedial exam required.";
         }
     } else if(grade >= 50) {
         result = "remedial exam required";
     } else {
         result = "Fail";
     }
-  
- alert("Student grade: " + grade + " Attendance: " + attendance + "Final project submitted: " + project + "Result" + result);
+   alert("Student grade: " + grade + " Attendance: " + attendance + " Final project submitted: " + project + " Result " + result);
 
- if (performance !== "----") {
+ if (performance == " 90") {
     alert("Performance level: Not defined");
  } else {
-    alert("Perfomance level: =====");
+    alert("Perfomance level: *** ");
  }
- 
-}
- 
+
+/* 
+================================================
+ Desktop Test 
+================================================
+grade = - 30;       attendance = 45;        project submitted = no;
+=> The grade should be between 0 to 100
+
+grade = 30;       attendance = hello;        project submitted = no;
+=> The attendance should be a number 
+
+grade = 56;       attendance = 78;        project submitted = no;
+=> Remedial exam required
+
+grade = 70;       attendance = 75;        project submitted = yes;
+=> PASS
+
+grade = 90;       attendance = 60;        project submitted = yes;
+=> Fail. Insufficient attendance
+
+grade = 90;       attendance = 90;        project submitted = no;
+=> ----
+
+ */
