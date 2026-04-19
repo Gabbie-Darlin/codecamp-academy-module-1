21f0d08720e0bee3f39c9504e59226156841e97b
Gabbie-Darlin <gabdarlin@gmail.com>
2026-04-19 14:09:39 +0000

    Code challenge js and flow chart

---
+++ class-63/code-challenge/code-challenge.js
@@ -0,0 +1,36 @@
+// Author: Gab-Darlin <Gabbie-Darlin>
+
+for (let count = 10; count > 0; count -- ) {
+    let newcount = count;
+    console.log("Count down: ", + newcount);
+}
+
+console.log("Lift off");
+
+/*
+=============================
+DESKTOP TEST
+=============================
+
+newcount = 10, Count down: 10, count --
+
+newcount = 9, Count down: 9, count --
+
+newcount = 8, Count down: 8, count --
+
+newcount = 7, Count down: 7, count --
+
+newcount = 6, Count down: 6, count --
+
+newcount = 5, Count down: 5, count --
+
+newcount = 4, Count down: 4, count --
+
+newcount = 3, Count down: 3, count --
+
+newcount = 2, Count down: 2, count --
+
+newcount = 1, Count down: 1, count --
+
+Lift off
+*/
