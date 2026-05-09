f0046f084c4e3408ebe40914c25e1bb2ac3de71c
Gabbie-Darlin <gabdarlin@gmail.com>
2026-05-09 23:31:51 +0000

    Code challenge solved

---
+++ class-79/examples/challenge.js
@@ -0,0 +1,26 @@
+const character = {
+    alias: "Hitman",
+    number: "47",
+    nationality: "Romanian",
+    gender: "male",
+    age: "35",
+    height: "6ft 2",
+    profession: "assassin",
+    affiliateAgency: "International Contract Agency (ICA)",
+    handler: {
+        name: "Diana Burnwood",
+        age: "40",
+        height: "5ft 4",
+        nationality: "American",
+        gender: "female",
+        isAlive: true,
+    },
+    isAlive: true,
+    signatureWeapons: "AMT Hardballer pistols",
+   creator: ["Dr Otto Wolfgang", "65", "Russian", "Genetics",]
+}
+
+
+console.log("****************");
+console.log(character);
+console.log("****************");
